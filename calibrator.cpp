9e5b976812f6c1b133d590d7a6370d76d3f36b87
Daniel Stahl <daniel.stahl@regions.com>
2018-01-14 14:53:48 -0600

    Added travis

--- calibrator.cpp
+++ calibrator.cpp
@@ -8,36 +8,7 @@
 constexpr int fullmodel=0;
 constexpr int hestonmodel=1;
 constexpr int bsmodel=2;
-/*
-template<typename CF, typename ConstraintFn, typename Array1, typename Array2>
-void fullModelCalibrator(
-    const CF& cf, const Array1& prices, const Array2& strikes, 
-    double S0, double r, double T, double xMax, int numU
-){
-    strikes.push_front(exp(xMax)*S0);
-    strikes.push_back(exp(-xMax)*S0);
-    auto results=calibrateoptions::l2normvector(
-        //these initial guesses may be a reasonable first guess but that this is highly dependent on the problem.   
-        std::vector<double>({.2, 2, 2, .4, .2, .3, .2, .2}), 
-        [&](const auto& localStrikes, const auto& args){
-            return optionprice::FangOostCallPrice(
-                S0, localStrikes, r, T, numU, 
-                cf(
-                    args[0], args[1], args[2], //c, g, m
-                    args[3], args[4], args[5], //y, sigma, speed
-                    args[6], args[7] //adaV, rho
-                )
-            );
-        }, 
-        [](const auto& args){ //C has to be positive.  Sigma has to be positive
-            return args[0]<0||args[4]<0;
-        }, 
-        [&](const auto& index){ //don't count first or last strike
-            return index==0||index==(strikes.size()-1)?0.0:1.0;
-        }, 
-        prices, strikes
-    );
-}*/
+
 template<typename CF, typename ConstraintFn, typename Array1, typename Array2>
 auto genericCallCalibrator(
     const CF& cf, const ConstraintFn& constraintFn, 
@@ -98,7 +69,7 @@ int main(int argc, char* argv[]){
                         std::vector<double>({.2, 2, 2, .4, .2, .3, .2, .2}), //guess
                         prices, get_k_var(parsedJson),
                         options.S0, options.r, options.T, xMax, numU
-                    ),
+                    ), 
                     prices.size()
                 );
                 break;

2627a88af68c2945d69df3f7785e3c78bc72c994
Daniel Stahl <danstahl1138@gmail.com>
2018-04-22 09:03:54 -0500

    updated scripts

--- scripts/tagAndRelease.sh
+++ scripts/tagAndRelease.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]
     exit
 fi
 git add .
-git commit -m "release v$1"
-git tag -a "v$1" -m "updating to v$1"
+git commit -m "release $1"
+git tag -a "$1" -m "updating to $1"
 git push --follow-tags origin master

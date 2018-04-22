17eacb897dda8c5d2bbf6b0644e7575610beff02
Daniel Stahl <danstahl1138@gmail.com>
2018-04-22 09:40:17 -0500

    release v1

--- scripts/mergeYml.js
+++ scripts/mergeYml.js
@@ -1,8 +1,22 @@
 const yaml = require('js-yaml')
 const {promisify} = require("es6-promisify");
 const readFile = promisify(require('fs-extra').readFile)
+const writeFile = promisify(require('fs-extra').writeFile)
+const loadYml=data=>new Promise((resolve, reject)=>{
+    yaml.safeLoadAll(data, asJson=>{
+        resolve(asJson)
+    })
+})
+readFile('../serverless.yml', 'utf8').then(loadYml).then(doc=>{
+    //console.log(doc)
+    const newFncs=Object.keys(doc.functions).reduce((aggr, funcName)=>{
+        return {...aggr, [funcName+'v1']:doc.functions[funcName]}
+    }, {})
+    const yml=yaml.safeDump({...doc, functions:newFncs})
 
-readFile('test.yml', 'utf8').then(result=>{
-    yaml.safeLoad()
+    return writeFile('./testYML.yml', yml)
+
+}).catch(err=>{
+    console.log(err)
 })
 

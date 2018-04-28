e761d5fc266837d39a444ea9f40439d743b28390
Daniel Stahl <danstahl1138@gmail.com>
2018-04-28 15:06:31 -0500

    release v2

--- scripts/tagAndRelease.sh
+++ scripts/tagAndRelease.sh
@@ -6,8 +6,7 @@ if [ -z "$1" ]
 fi
 git add .
 git commit -m "release $1"
-if git tag -a "$1" -m "updating to $1";
-then
+if git tag -a "$1" -m "updating to $1"; then
   git push --follow-tags origin master
 else
   echo "Tag already exists.  Overwrite? (Y/N)"
@@ -15,7 +14,13 @@ else
   if [ $contin==Y ]; then
     git tag -d $1
     git push --delete origin $1
-    git tag -a "$1" -m "updating to $1";
+    git tag -a "$1" -m "updating to $1"
+
+    id=curl -s https://api.github.com/repos/phillyfan1138/levy-functions/releases/tags/$1 | node -pe "JSON.parse(require('fs').readFileSync('/dev/stdin').toString()).id"
+
+    curl -i -u phillyfan1138 -H "User-Agent: levy-functions" -X DELETE https://api.github.com/repos/phillyfan1138/levy-functions/releases/$id
+    
     git push --follow-tags origin master
+
   fi
 fi

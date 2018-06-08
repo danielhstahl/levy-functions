b3709c1822fca374397208855daac1cf8ff37b87
Daniel Stahl <B0GX3@regions.com>
2018-06-08 12:05:22 -0500

    fixed key name

--- tests/unit/handler.test.js
+++ tests/unit/handler.test.js
@@ -23,10 +23,10 @@ const calibratorParams={
     "k":[95,130,150,160,165,170,175,185,190,195,200,210,240,250],
     "prices":[85,51.5,35.38,28.3,25.2,22.27,19.45,14.77,12.75,11,9.35,6.9,2.55,1.88]
 }
-const createEvent=(data, parameters, queryParameters)=>({
+const createEvent=(data, parameters, queryStringParameters)=>({
     body:JSON.stringify(data),
     pathParameters:parameters,
-    queryParameters
+    queryStringParameters
 })
 it('correctly calls calculator handlers', (done)=>{
     const event=createEvent({
